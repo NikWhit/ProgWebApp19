ca4f9066bf0895caf5b7fbcf3da67b1ade64245e
NikWhit <135679785+NikWhit@users.noreply.github.com>
2023-12-13 16:18:21 -0700

    code upload

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,39 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+console.error('putDb not implemented');
+//src 19-PWA/23-Ins_IndexedDB-CRUD/database.js #Src
+
+console.log('Post to the database');
+
+  // Create a connection to the database database and version we want to use.
+  const todosDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = todosDb.transaction('jate', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .add() method on the store and pass in the content.
+  const request = store.put({ id: 1, value: content  });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {console.error('getDb not implemented');
+  console.log('Get from Database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  console.log(result);
+  return result;
+};
 
 initdb();
